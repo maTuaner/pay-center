47e43acfbf7e30ae21c3551fb6342c129e94eea6
liuchang <liuchang@shanhai.team>
2022-07-14 16:11:54 +0800

    init

---
+++ demo/src/main/java/com/hkuo/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.hkuo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuch
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
